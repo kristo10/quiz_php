e89210ee9b1a2f075eac10feaed7d3ae6f6f24d1
kristo <tarifakristo7023@gmail.com>
2023-01-17 17:57:10 +0100

    first

---
+++ quiz.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	
+	<title>PHP QUIZ</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+ 
+<body>
+ 
+	<div id="page-wrap">
+ 
+	<h1>Quiz</h1>
+
+	<form action="result.php" method="post" id="quiz">
+		
+            <ol>
+            
+                <li>
+                
+                    <h3>Cfare lende te pelqen?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Matematika </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Fizika</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) Biologjia</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) Kimia</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Sa i perkushtuar jeni?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Aspak</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Pak</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Mjaftueshem</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) Shume</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>A kane dikuar prinderit ne perzgjedhjen e lendes suaj te preferuar?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Aspakt</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Pak</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Mjaftueshem</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) Shume</label>
+                    </div>
+                
+                </li>
+                
+                
+            </ol>
+            
+            <input type="submit" value="Submit" class="submitbtn" />
+		
+		</form>
+	
+	</div>
+ 
+ 
+</body>
+ 
+</html>
